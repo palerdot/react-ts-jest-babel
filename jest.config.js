45523392574989969179dc63c45d77f79d84d543
Ahn <ahn@Ahns-MacBook-Pro.local>
2019-11-10 01:30:50 +0100

    first commit

---
+++ jest.config.js
@@ -0,0 +1,53 @@
+module.exports = {
+  /**
+   * You can use jsWithBabel preset provided by ts-jest so you don't need to define transform here. The preset jsWithBabel
+   * will automatically define "^.+\\.jsx?$": "babel-jest" for you
+   */
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '<rootDir>/config/jest/fileTransform.js',
+  },
+  'roots': [
+    '<rootDir>/src',
+  ],
+  'collectCoverageFrom': [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+  ],
+  'setupFiles': [
+    'react-app-polyfill/jsdom',
+  ],
+  'setupFilesAfterEnv': [],
+  'testMatch': [
+    '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}',
+  ],
+  'testEnvironment': 'jest-environment-jsdom-fourteen',
+  'transformIgnorePatterns': [
+    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+  'modulePaths': [],
+  'moduleNameMapper': {
+    '^react-native$': 'react-native-web',
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+  },
+  'moduleFileExtensions': [
+    'web.js',
+    'js',
+    'web.ts',
+    'ts',
+    'web.tsx',
+    'tsx',
+    'json',
+    'web.jsx',
+    'jsx',
+    'node',
+  ],
+  'watchPlugins': [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
+};
