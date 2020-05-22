cfa2fc1463b62febf6ec7af6a2b6f26b44270be5
Ahn <anhpnnd@gmail.com>
2020-05-22 12:17:05 +0200

    chore: add react-test-renderer

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,12 +3,10 @@ import Title from './components/title';
 import renderer from 'react-test-renderer';
 import { render } from '@testing-library/react';
 import App from './App';
-import styles from './custom-style.scss';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
-  expect(styles).toBeDefined();
   expect(linkElement).toBeInTheDocument();
 });
 
